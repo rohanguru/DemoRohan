3e9371c51facddf614ce50615780a95c253c1ccd
rohanguru <121804442+rohanguru@users.noreply.github.com>
2023-02-07 11:58:30 +0530

    All Good

--- force-app/main/default/lwc/gitsearchBox/gitsearchBox.js
+++ force-app/main/default/lwc/gitsearchBox/gitsearchBox.js
@@ -1,7 +1,10 @@
 import { LightningElement,wire} from 'lwc';
 import { publish, MessageContext } from 'lightning/messageService';
 import searchMessage from '@salesforce/messageChannel/searchGitChannel__c';
+import Pikachu from '@salesforce/resourceUrl/Pikachu2';
+
 export default class gitSearchBox extends LightningElement {
+    imageUrl = Pikachu;
     @wire(MessageContext)
     messageContext;
     handleClick(event){
---
+++ force-app/main/default/lwc/ticTacToe/ticTacToe.js
@@ -0,0 +1,25 @@
+import { LightningElement, track } from 'lwc';
+
+const ROWS = [
+  [{ index: 0, value: '' }, { index: 1, value: '' }, { index: 2, value: '' }],
+  [{ index: 3, value: '' }, { index: 4, value: '' }, { index: 5, value: '' }],
+  [{ index: 6, value: '' }, { index: 7, value: '' }, { index: 8, value: '' }],
+];
+
+export default class TicTacToe extends LightningElement {
+  @track rows = JSON.parse(JSON.stringify(ROWS));
+  xIsNext = true;
+
+  handleCellClick(event) {
+    const cellIndex = event.target.dataset.index;
+    const currentRow = Math.floor(cellIndex / 3);
+    const currentCell = cellIndex % 3;
+
+    if (this.rows[currentRow][currentCell].value !== '') {
+      return;
+    }
+
+    this.rows[currentRow][currentCell].value = this.xIsNext ? 'X' : 'O';
+    this.xIsNext = !this.xIsNext;
+  }
+}
